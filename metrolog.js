69e2e4e3a6bdb20a3fe9fd88077bfcc417c75e7a
ave <ave6990@ya.ru>
2021-04-22 06:17:55 +0000

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   metrolog.js
    	new file:   package.json
    	modified:   spangas.js

--- metrolog.js
+++ metrolog.js
@@ -1,26 +1,45 @@
-const range_converter = (s_min, s_max, d_min, d_max) => {
+const rangeConverter = (s_min, s_max, d_min, d_max) => {
     return (value) => {
-		return (value -s_min) * (d_max - d_min) / (s_max - s_min) + d_min;
-    };
+		return (value -s_min) * (d_max - d_min) / (s_max - s_min) + d_min
+    }
 }
 
-const relative_error = (d_val, s_val) => {
-    return (d_val - s_val) / s_val * 100;
+const relativeError = (d_val, s_val) => {
+    return (d_val - s_val) / s_val * 100
 }
 
-const reduced_error = (d_val, s_val, n_val) => {
-    return (d_val - s_val) / n_val * 100;
+const reducedError = (d_val, s_val, n_val) => {
+    return (d_val - s_val) / n_val * 100
 }
 
-const converter_to_nc = (temp, pres) => {
+const volumeToNc = (temp, pres) => {
 	return (val) => {
 		return val * pres * 293.2 /
-			((273.2 + temp) * 101.3);
-	};
+			((273.2 + temp) * 101.3)
+	}
 }
 
 const v100nc = (temp, pres) => {
-	return 100 * (273.2 + temp) * 101.3 / (pres * 293.2);
+	return 100 * (273.2 + temp) * 101.3 / (pres * 293.2)
 }
 
-module.exports = { v100nc, range_converter, relative_error, reduced_error, converter_to_nc }
+const sko = (values) => {
+	let n = values.length
+	let nc = 0
+	nc = values.reduce((res, val) => {
+		return Number(res) + Number(val)
+	}) / n
+	let vals = values.map((val) => {
+		return (val - nc) ** 2
+	})
+	return precision((vals.reduce((res, val) => {
+		return res + val
+	}) / (n - 1)) ** 0.5)
+}
+
+const precision = (val) => {
+    // precision set to 12 signs after comma.
+    parseFloat(val.toFixed(12)) 
+}
+
+module.exports = { v100nc, rangeConverter, relativeError, reducedError, volumeToNc, sko }
--- spangas.js
+++ spangas.js
@@ -1,50 +1,45 @@
 class SpanGas {	
-	constructor(c, t) {
-		this.conc = c
-		this.tolerance = t
+//    static id = 0 // don't work
+
+	constructor(conc, limit, name) {
+		this.conc = conc
+		this.limit = limit
+        SpanGas.id++ 
+        this.name = name || `SpanGas#${SpanGas.id}`
 	}
 
 	get min() {
-		return this.conc - this.tolerance
+		return this.conc - this.limit
 	}
 
 	get max() {
-		return this.conc + this.tolerance
+		return this.conc + this.limit
 	}
 
 	get len() {
 		return this.max - this.min
 	}
+
+    static sort(gas1, gas2) {
+        return gas1.min - gas2.min
+    }
+
+    isIntersect(gas1, gas2) {
+    	let temp = sort(gas1, gas2)
+    	if (temp[0].max >= temp[1].min) {
+    		return true
+    	}
+    	return false
+    }
 }
 
-const sort = (...span) => {
-	return span.sort((x, y) => {
-			return _fsort(x.min, y.min)
-		})
-}
+SpanGas.id = 0
 
-const intersect = (gas1, gas2) = > {
-	let temp = sort(gas1, gas2)
-	if (temp[0].max >= temp[1].min) {
-		return true
-	}
-	return false
-}
 
-const intersection = (...rng) => {
-	temp = sort(rng)
-}
+const g1 = new SpanGas(2.2, 0.1)
+const g2 = new SpanGas(1.1, 0.05)
+const g3 = new SpanGas(4.4, 0.1)
+console.log(g1, g2, g3)
+console.log([g1, g2, g3].sort(SpanGas.sort))
 
-const _fsort = () => {
-	return (x, y) => {
-		if (x<y) {
-			return -1
-		}
-		else if (x>y) {
-			return 1
-		}
-		else {
-			return 0
-		}
-	}
-}
+module.exports = SpanGas
