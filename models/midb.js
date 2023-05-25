1183660947b80f726c75868fd25c9640a56c981d
ave6990 <ave6990@ya.ru>
2023-05-25 17:31:20 +0500

    some troubles with async

--- models/midb.js
+++ models/midb.js
@@ -1,11 +1,34 @@
 import sqlite3 from 'sqlite3'
 import { open } from 'sqlite'
 
+/*// midb.db.then(db => db.all('select * from mi limit 1')).then(console.log)
 const db = (async () => {
-    return await open({
+    return open({
         filename: './data/midb.db',
         driver: sqlite3.Database
     })
-})()
+})() */
 
-export { sqlite3, db }
+class MIdb {
+    constructor() {
+        this.connect('./data/midb.db')
+    }
+
+    async connect(filename) {
+        try {
+            this.db = await open({
+                filename: filename,
+                driver: sqlite3.Database
+            })
+        } catch (err) {
+            console.log('Connection Error!\n', err)
+    }
+
+    async getCondition(date) {
+        return await this.db.all(`select * from conditions where date = '${date}'`)
+    }
+}
+
+const mi = new MIdb()
+
+export { mi }
